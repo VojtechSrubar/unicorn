63cf85555038cfb2f2b629beaca7466065735984
Vojta Srubar <v.srubar.st@spseiostrava.cz>
2024-05-24 13:14:21 +0200

    GIPHY PROJEKT

--- giphyproject/src/components/ClipsGiphySection.js
+++ giphyproject/src/components/ClipsGiphySection.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ClipsGiphySection.css";
 
-const ClipsGiphySection = () => {
+const ClipsGiphySection = ({ giphysArray }) => {
+  const [giphys, setGiphys] = useState([]);
+
+  const randomizeData = (giphysArray) => {
+    return giphysArray.sort(() => Math.random() - 0.5);
+  };
+
+  useEffect(() => {
+    let randomData = randomizeData(giphysArray);
+    setGiphys(randomData.slice(0, 3));
+  }, [giphysArray]);
+
   return (
     <div className="clips-grid">
-      <div className="first-column"></div>
-      <h2>sds</h2>
-      <div className="second-column">
-        <h2>sds</h2>
-        <h2>sds</h2>
-      </div>
+      {giphys && giphys.lenght ? (
+        <>
+          <div className="first-column"></div>
+          <img src={giphys?.[0]?.images.downsized.url} />
+          <div className="second-column">
+            <img src={giphys?.[1]?.images.downsized.url} />
+            <img src={giphys?.[2]?.images.downsized.url} />
+          </div>
+        </>
+      ) : null}
     </div>
   );
 };
