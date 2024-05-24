a2e4ce0fddbd6e426b01993c613b2ecccfd4f0fa
Vojta Srubar <v.srubar.st@spseiostrava.cz>
2024-05-24 09:46:49 +0200

    GIPHY PROJEKT

--- giphyproject/src/App.js
+++ giphyproject/src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
+import "./App.css";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
       <div className="main">
-      <Header/>
-      <h1>Search Contaier</h1>
-      <h1>Média</h1>
-    </div>
+        <Header />
+        <h1>Search Contaier</h1>
+        <h1>Média</h1>
+      </div>
     </div>
   );
 }
--- giphyproject/src/components/Header.js
+++ giphyproject/src/components/Header.js
@@ -1,31 +1,43 @@
-import React from 'react'
-import "./Header.css"
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import React from "react";
+import "./Header.css";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 export default function Header() {
   return (
-    <div className='header'>
-        <img src="./images/giphyLogo.png" alt="logo" />
-        <div className='menu'>
-            <h2>Reakce</h2>
-            <h2>Zábava</h2>
-            <h2>Sporty</h2>
-            <h2>Stickery</h2>
-            <h2>Umělci</h2>
-            <MoreVertIcon/>
+    <div className="header">
+      <img src="./images/giphyLogo.png" alt="logo" />
+      <div className="menu">
+        <div className="button-wrapper reakce">
+          <h2>Reakce</h2>
         </div>
-    <div className='button'>
-    <h2>Upload</h2>
-    </div>
-    <div className='button'>
+        <div className="button-wrapper zabava">
+          <h2>Zábava</h2>
+        </div>
+        <div className="button-wrapper sporty">
+          <h2>Sporty</h2>
+        </div>
+        <div className="button-wrapper stickery">
+          <h2>Stickery</h2>
+        </div>
+        <div className="button-wrapper umelci">
+          <h2>Umělci</h2>
+        </div>
+        <div className="button-wrapper moreverticon">
+          <MoreVertIcon />
+        </div>
+      </div>
+      <div className="button">
+        <h2>Upload</h2>
+      </div>
+      <div className="button">
         <h2>Create</h2>
-    </div>
-    <div className='profile'>
-        <img src="./images/avatar.png" alt="avatar"/>
+      </div>
+      <div className="profile">
+        <img src="./images/avatar.png" alt="avatar" />
         <h2>Vojta</h2>
-        <KeyboardArrowDownIcon/>
+        <KeyboardArrowDownIcon />
+      </div>
     </div>
-    </div>
-  )
+  );
 }
