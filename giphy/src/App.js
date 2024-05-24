b5988837bb19bc4cc01ca306ca42bbf62a66d1fb
Vojta Srubar <v.srubar.st@spseiostrava.cz>
2024-05-24 08:42:17 +0200

    react

---
+++ giphy/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="main">
+      <Header/>
+      <h1>Search Contaier</h1>
+      <h1>Média</h1>
+    </div>
+    </div>
+  );
+}
+
+export default App;
