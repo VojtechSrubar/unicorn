34d977ba722c41192203b8156d9e259f76f2709e
Vladislav Perdoch <v.perdoch,st@spseiostrava.cz>
2024-05-27 13:47:07 +0200

    Product css

---
+++ eshop/src/componets/ProductList.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link, Outlet } from "react-router-dom";
+import Product from "../componets/Product";
+import { fetchProducts } from "../pages/Functions";
+
+
+const ProductList = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+      fetchProducts().then((json) => setProducts(json));
+    }, []);
+  
+    return (
+      <>
+        <Outlet />
+        <div className="container">
+          <div className="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-3">
+            {products.map((product) => (
+              <Link key={product.id} to={`/products/${product.id}`}>
+                <div className="col">
+                  <Product {...product} />
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </>
+    );
+}
+
+export default ProductList
--- eshop/src/pages/ProductsPage.js
+++ eshop/src/pages/ProductsPage.js
@@ -1,34 +1,12 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import { Link, Outlet } from "react-router-dom";
-
 import Header from "../componets/Header";
-import Product from "../componets/Product";
-
-import { fetchProducts } from "./Functions";
+import ProductList from "../componets/ProductList";
 
 function ProductsPage() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    fetchProducts().then((json) => setProducts(json));
-  }, []);
-
   return (
     <>
       <Header />
-      <Outlet />
-      <div className="container">
-        <div className="row row-cols-1 row-cols-md-3 g-4">
-          {products.map((product) => (
-            <Link key={product.id} to={`/products/${product.id}`}>
-              <div className="col">
-                <Product {...product} />
-              </div>
-            </Link>
-          ))}
-        </div>
-      </div>
+      <ProductList/>
     </>
   );
 }
