4fabc6ede8b48617fce8cf9f89aed3a0a2d11430
Jan Novotny <j.novotny.st@spseiostrava.cz>
2024-05-27 11:58:00 +0200

    productsPage

--- eshop/src/App.js
+++ eshop/src/App.js
@@ -1,31 +1,16 @@
-import './App.css';
+import "./App.css";
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Product from './componets/Product';
+import Product from "./componets/Product";
 import Button from "./componets/Button";
 import Header from "./componets/Header";
-import { fetchProducts } from './utils/functions';
+import { fetchProducts } from "./utils/functions";
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetchProducts().then(json => setData(json));
-  }, []);
 
   return (
     <>
-    <div className="container">
-      <div className="row row-cols-1 row-cols-md-3 g-4">
-        {data.map(product => (
-          <div className="col" key={product.id}>
-            <Product {...product} />
-          </div>
-        ))}
-      </div>
-    </div>
-      <Header />
       <Button>Button</Button>
     </>
   );
--- eshop/src/pages/ProductsPage.js
+++ eshop/src/pages/ProductsPage.js
@@ -3,11 +3,26 @@ import { Link } from "react-router-dom";
 import { fetchProducts } from "../utils/functions";
 
 function ProductsPage() {
-  
-  const products = fetchProducts();
-  console.log(products);
+  const [products, setProducts] = useState([]);
 
-  return <div>ProductsPage</div>;
+  useEffect(() => {
+    fetchProducts().then((json) => setProducts(json));
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+          {products.map((product) => (
+            <div className="col" key={product.id}>
+              <Product {...product} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default ProductsPage;
