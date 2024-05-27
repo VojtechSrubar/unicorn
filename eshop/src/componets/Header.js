431a06669fe2122a1a211b56c58c2cb91d58d039
VojtechSrubar <v.srubar.st@spseiostrava.cz>
2024-05-27 10:46:50 +0200

    Header update

--- eshop/src/componets/Header.js
+++ eshop/src/componets/Header.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./Header.css";
 
 export default function Header() {
-  <h1>HEADER</h1>;
+  return (
+    <div className="header">
+      <div className="navbar">
+        <div className="home">
+          <h2>Home</h2>
+        </div>
+        <div className="contacts">
+          <h2>Contacts</h2>
+        </div>
+        <div className="products">
+          <h2>Products</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
