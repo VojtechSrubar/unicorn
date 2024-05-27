b013b973818be5597eb800dab928cb58b3f276c0
Vladislav Perdoch <v.perdoch,st@spseiostrava.cz>
2024-05-27 11:34:24 +0200

    uprava

--- eshop/src/App.js
+++ eshop/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import Product from './componets/Product';
@@ -12,11 +11,9 @@ const fetchData = async () => {
     console.error(error);
   }
 };
-=======
 import "./App.css";
 import Button from "./componets/Button";
 import Header from "./componets/Header";
->>>>>>> e481859de5c433e9bc21cad6e1ec8be219cf79c9
 
 function App() {
   const [data, setData] = useState([]);
@@ -26,7 +23,7 @@ function App() {
   }, []);
 
   return (
-<<<<<<< HEAD
+    <>
     <div className="container">
       <div className="row row-cols-1 row-cols-md-3 g-4">
         {data.map(product => (
@@ -36,12 +33,9 @@ function App() {
         ))}
       </div>
     </div>
-=======
-    <>
       <Header />
       <Button>Button</Button>
     </>
->>>>>>> e481859de5c433e9bc21cad6e1ec8be219cf79c9
   );
 }
 
