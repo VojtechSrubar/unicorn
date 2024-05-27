b5249db8b87a9b903add9c987f3fc232a9425745
VojtechSrubar <v.srubar.st@spseiostrava.cz>
2024-05-27 10:41:43 +0200

    HEADER

--- eshop/src/App.js
+++ eshop/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Button from "./componets/Button";
+import Header from "./componets/Header";
 
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <Header />
       <Button>Button</Button>
     </>
   );
---
+++ eshop/src/componets/Header.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  <h1>HEADER</h1>;
+}
