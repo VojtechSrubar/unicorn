b7b892fd5aa98b7bf59cdafb8386a27ff1de2e9f
Jan Novotny <j.novotny.st@spseiostrava.cz>
2024-05-27 11:15:43 +0200

    NotFoundPage

--- eshop/src/pages/NotFoundPage.js
+++ eshop/src/pages/NotFoundPage.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function NotFoundPage() {
   return (
-    <div>NotFoundPage</div>
+    <div>
+      <p>Page not found</p>
+      <Link to="/">Go back</Link>
+    </div>
   )
 }
 
