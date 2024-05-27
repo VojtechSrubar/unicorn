4edf42811e0dd4ff619d86ffa8dab954fe37b708
Vladislav Perdoch <v.perdoch,st@spseiostrava.cz>
2024-05-27 10:27:21 +0200

    bootstrap

--- eshop/src/App.js
+++ eshop/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Button from './componets/Button';
+
 
 function App() {
   return (
     <>
+      
       <h1>Hello World!</h1>
+      <Button></Button>
     </>
   );
 }
---
+++ eshop/src/componets/Button.js
@@ -0,0 +1,10 @@
+import './Button.css'
+import React from 'react'
+
+const Button = () => {
+  return (
+    <button type="button" class="btn btn-primary">Primary</button>
+  )
+}
+
+export default Button
