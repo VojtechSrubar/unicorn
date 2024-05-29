98536949e3d9b8c60f2426719d4592367cd4cae6
Jan Novotny <j.novotny.st@spseiostrava.cz>
2024-05-29 13:37:00 +0200

    Responzovita

--- eshop/src/pages/HomePage.js
+++ eshop/src/pages/HomePage.js
@@ -3,25 +3,43 @@ import { Link } from "react-router-dom";
 import "./HomePage.css";
 import hand_icon from "../assets/hand_icon.png";
 import arrow from "../assets/arrow.png";
+import Product from "../componets/Product";
 
 const HomePage = () => {
+  const product = {
+    id: 1,
+    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price: 109.95,
+    description:
+      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    category: "men's clothing",
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    rating: {
+      rate: 3.9,
+      count: 120,
+    },
+  }
+
   return (
     <div className="hero">
       <div className="hero-left">
         <h2>NEW ARRIVALS ONLY</h2>
         <div className="hand-hand-icon">
           <img src={hand_icon} alt="hand" />
-          <p>new</p>
-          <p>products</p>
+          <p>New</p>
+          <p>Products</p>
         </div>
         <Link to="/products" className="hero-latest">
-          Latest arrivals
+          Latest Arrivals
           <img src={arrow} alt="arrow" />
         </Link>
       </div>
-      <div className="hero-right">
+      {/* <div className="hero-right">
         <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
-      </div>
+      </div> */}
+      <Link>
+        <Product {...product} />
+      </Link>
     </div>
   );
 };
