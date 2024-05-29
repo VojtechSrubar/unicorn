0c38a6c699edbf765b41ff3b43240735947bf0c4
Vladislav Perdoch <v.perdoch,st@spseiostrava.cz>
2024-05-29 10:12:59 +0200

    products css

--- eshop/src/componets/Product.js
+++ eshop/src/componets/Product.js
@@ -5,7 +5,7 @@ import "./Product.css";
 
 const Product = ({ id, image, title, description, price }) => {
   const truncatedDescription = description && description.length > 100
-    ? `${description.substring(0, 150)}...`
+    ? `${description.substring(0, 300)}...`
     : description;
 
   return (
@@ -14,13 +14,19 @@ const Product = ({ id, image, title, description, price }) => {
         <img src={image} className="product-image" alt={title} />
         <div className="product-info">
           <h5 className="product-title">{title}</h5>
-          <p className="product-description">{truncatedDescription || 'No description available'}</p>
+          <p className="product-description">
+            {truncatedDescription ? (
+              truncatedDescription
+                .replace(/\//g, " / ") // Replace forward slashes with space-slash
+                .replace(/,/g, ", ") // Replace commas with space-comma
+            ) : (
+              'No description available'
+            )}
+          </p>
           <p className="product-price">${price}</p>
-          
+          <Button />
         </div>
-        <Button />
       </div>
-      
     </div>
   );
 };
